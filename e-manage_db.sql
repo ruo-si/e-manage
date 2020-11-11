7b7a397ff2fe822a4a9c18069748f86ec70b236d
Victoria Lee <victoriacmlee@gmail.com>
2020-11-10 21:40:47 -0500

    updated schema

--- e-manage_db.sql
+++ e-manage_db.sql
@@ -6,13 +6,11 @@ USE e-manage_db;
 
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(45) NOT NULL,
-  price DECIMAL(10,2) NULL,
-  quantity INT NULL,
+  name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
 
-CREATE TABLE roles (
+CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL(10,2)NOT NULL,
@@ -22,9 +20,14 @@ CREATE TABLE roles (
 
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(45) NOT NULL,
-  last_name VARCHAR(45) NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
   manager_id INT,
-  PRIMARY KEY (id)
+  PRIMARY KEY (id) NULL,
 );
+
+
+SELECT * FROM department;
+SELECT * FROM roles;
+SELECT * FROM employee;
