0e2a41c489c111518ab9346eab8bd7c881c82208
Victoria Lee <victoriacmlee@gmail.com>
2020-11-29 08:34:59 -0500

    start new pseudo code -- consolidate and clean up code structure

---
+++ pseudo-update.js
@@ -0,0 +1,45 @@
+// dependencies 
+
+// connection
+
+// app start
+
+    // ask (inquirer)
+
+        // add (department, role, employee)
+        // view (department, role, employee)
+        // update (department, role, employee)
+        // remove (department, role, employee)
+
+        // switch statement redirect
+
+// function Add
+    // ask which table (department, role, employee)
+
+    // connection.query (INSERT INTO)
+
+
+// function view
+    // ask which table 
+
+    // connection.query SELECT)
+
+
+// function update
+    // ask which 
+
+    // connection.query (UPDATE...SET)
+
+
+// function remove
+    // ask which 
+
+    // connection.query (DELETE)
+
+
+// function exit
+
+    // connection.end()
+
+
+
